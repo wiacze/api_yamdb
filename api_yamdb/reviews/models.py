e9d29e42feaa3593ced4d8450d50beac5d497a55
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-08-28 15:11:03 +0500

    add models for Category, Genre and Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Модель категории."""
+    name = models.CharField(
+        verbose_name='Название группы',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор группы',
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.'),
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """Модель жанра."""
+    name = models.CharField(
+        verbose_name='Название жанра',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор жанра',
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.'),
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    """Модель произведения"""
+    name = models.CharField(
+        verbose_name='Название произведения',
+        max_length=256
+    )
+    description = models.TextField(
+        verbose_name='Описание произведения'
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор произведения',
+        help_text=('Идентификатор страницы для URL; разрешены символы '
+                   'латиницы, цифры, дефис и подчёркивание.'),
+        unique=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        related_name='titles',
+        on_delete=models.CASCADE
+    )
+    # Создание связующей таблицы title_genre для отношения N-M
+    genre = models.ManyToManyField(Genre)
