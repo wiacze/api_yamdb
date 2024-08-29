9a7006409df710fb998dbfabd0777ae7a478a712
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-08-29 21:16:40 +0500

    insert func, import presets

--- api_yamdb/reviews/management/commands/csv_import.py
+++ api_yamdb/reviews/management/commands/csv_import.py
@@ -1,9 +1,41 @@
 import csv
-
+import os
 from django.apps import apps
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from reviews.models import Category, Genre, Title
+
+
+IMPORT_DATA = {
+    'users': {
+        'models': [
+            'CustomUser'
+        ],
+        'csv_files': [
+            'users.csv'
+        ],
+    },
+    'reviews': {
+        'models': [
+            'Category',
+            'Genre',
+            'Title',
+            'title_genre',
+            'Review',
+            'Comment'
+        ],
+        'csv_files': [
+            'category.csv',
+            'genre.csv',
+            'titles.csv',
+            'genre_title.csv',
+            'review.csv',
+            'comments.csv'
+        ],
+    },
+}
+
+FILE_PATH = '././static/data/'
 
 
 class Command(BaseCommand):
@@ -11,56 +43,79 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'app_name',
+            '--app',
             type=str,
-            default='reviews',
             help='Name of the Django app containing the model.'
         )
+
         parser.add_argument(
-            'model_name',
-            nargs='*',
+            '--model',
             type=str,
-            default=[
-                'category',
-                'genre',
-                'title',
-                'title_genre',
-            ],
-            help='Name of the Django model.'
+            help='Name of the Django model(s). Can be specified multiple times.'
         )
+
         parser.add_argument(
-            'csv_file',
-            nargs='*',
+            '--file',
             type=str,
-            default=[
-                'category.csv',
-                'genre.csv',
-                'titles.csv',
-                'genre_title.csv',
-            ],
-            help='Path to the CSV file.'
+            help='Path to the CSV file(s). Can be specified multiple times.'
         )
+
+    def check_input(self, app, model=None, file=None):
+        app_names = [app_config.name for app_config in apps.get_app_configs()]
+        if app in app_names:  # Проверяем существование app в приложениях
+            if model:  # Если передан model -> проверка существования
+                try:
+                    model = apps.get_model(
+                        app_label=app,
+                        model_name=model
+                    )
+                except LookupError:
+                    raise CommandError(f'App "{app}" '
+                                       f'doesn`t have model "{model}"')
+        else:
+            raise CommandError(f'App: {app} doesn`t exists')
+
+        if file:  # Проверяем, задан ли файл
+            if not os.path.exists(file):
+                raise CommandError(f'File: {file} doesn`t exists '
+                                   f'at {FILE_PATH}')
+
+        return True
+
+    def insert(self, app, model, file):
+        pass
 
     def handle(self, *args, **options):
-        app_name = options['app_name']
-        model_names = options['model_name']
-        csv_files = options['csv_file']
-
-        for model_name, csv_file in zip(model_names, csv_files):
-            file_path = f'././static/data/{csv_file}'
+        '''Отрабатывает импорт данных.'''
+        '''
+        Если переданы аргументы app, model, file -> точечная вставка.
+        Если передан аргумент app -> произвести вставку по заготовленным
+        пресетам для приложения.
+        Если аргументы не переданы -> вставка по пресету:
+        приложение - модель - файл
+        '''
+        app_name = options['app']
+        model_name = options['model']
+        csv_file = os.path.join(FILE_PATH, options['file'])
+        print(app_name, model_name, csv_file)
+        print(self.check_input(app_name, model_name, csv_file))
+        if app_name is None and model_name is None and csv_file is None:
+            return ('All is None')
+        for model_name, csv_file in zip(model_name, csv_file):
 
             try:
                 model = apps.get_model(
                     app_label=app_name,
                     model_name=model_name
                 )
+                print(model.__name__, app_name)
             except LookupError:
                 self.stderr.write(self.style.ERROR(
                     f'Model "{model_name}" not found in app "{app_name}".'
                 ))
                 continue
 
-            with open(file_path, 'r', encoding='utf-8') as csvfile:
+            with open(csv_file, 'r', encoding='utf-8') as csvfile:
                 reader = csv.DictReader(csvfile)
 
                 for row in reader:
@@ -79,4 +134,3 @@ class Command(BaseCommand):
 
                     model.objects.update_or_create(**row)
 
-        return None
