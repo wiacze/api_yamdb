f508324385dfae0209374841ac9cf3ecd4647360
Anton Savchenko <nvbest63@yandex.ru>
2024-08-31 09:55:39 +0500

    added genre serializer, view, router

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -2,11 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.v1.views.category_view import CategoryViewSet
+from api.v1.views.genre_view import GenreViewSet
 
 
 router_v1 = DefaultRouter()
 
 router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
 
 
 urlpatterns = [
