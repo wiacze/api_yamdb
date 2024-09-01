efa14db9bf6d2e81f6c673a2168ea7a7f58e0486
KamilVysotskij <160237836+KamilVysotskij@users.noreply.github.com>
2024-09-01 17:00:21 +0500

    Update user_serializer.py

--- api_yamdb/api/v1/serializers/user_serializer.py
+++ api_yamdb/api/v1/serializers/user_serializer.py
@@ -36,4 +36,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'username', 'email', 'first_name', 'last_name', 'bio', 'role')
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
