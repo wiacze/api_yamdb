7ccf391d6f88b7aca4e9806f049f967ee8bc7cd1
KamilVysotskij <160237836+KamilVysotskij@users.noreply.github.com>
2024-09-01 17:00:54 +0500

    Update urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.v1.views.user_view import (
-    UserViewSet, GetTokenViewSet, UserCreateViewSet)
+from api.v1.views.user_view import (UserViewSet,
+                                    GetTokenViewSet,
+                                    UserCreateViewSet)
 
 
 router_v1 = DefaultRouter()
