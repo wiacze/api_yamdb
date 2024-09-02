445b51f823df033b744754cb2282de70a21f96b0
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-02 17:35:43 +0300

    Add IsAdmin permission

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -1,6 +1,17 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and (
+                request.user.is_superuser
+                or request.user.is_staff
+                or request.user.is_admin
+            )
+        )
+
+
 class ReadOnlyOrIsAdmin(BasePermission):
 
     def has_permission(self, request, view):
