f80101366c7adc1de77a5c1e2cfaa729a76405f4
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-02 21:19:32 +0300

    Fix imports

--- api_yamdb/api/v1/utils.py
+++ api_yamdb/api/v1/utils.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 
-from api_yamdb.settings import EMAIL_YAMDB
+from api_yamdb.constants import EMAIL_YAMDB
 
 
 def send_confirmation_code(email, confirmation_code):
