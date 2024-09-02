6dcc838f1b9e0af85cfa29d1b43d2b00b5e4787f
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-02 17:35:09 +0300

    Add auth and users urls

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from api.v1.views.category_view import CategoryViewSet
 from api.v1.views.title_view import TitleViewSet
 from api.v1.views.genre_view import GenreViewSet
-
+from api.v1.views.user_view import SignUpViewSet, GetTokenViewSet, UserViewSet
 
 
 router = DefaultRouter()
@@ -12,8 +12,18 @@ router = DefaultRouter()
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
 router.register('titles', TitleViewSet, basename='titles')
+router.register('users', UserViewSet, basename='users')
 
+auth_urls = [
+    path(
+        'signup/', SignUpViewSet.as_view({'post': 'create'}), name='signup'
+    ),
+    path(
+        'token/', GetTokenViewSet.as_view({'post': 'create'}), name='token'
+    ),
+]
 
 urlpatterns = [
+    path('auth/', include(auth_urls)),
     path('', include(router.urls)),
 ]
