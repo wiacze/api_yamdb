210e19e5aa46827b8ef545cdba64268fa7bbccd8
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-09-03 22:37:20 +0500

    Update permissions with rights feature

--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -23,3 +23,17 @@ class ReadOnlyOrIsAdmin(BasePermission):
             return request.method in SAFE_METHODS or request.user.is_admin
         else:
             return request.method in SAFE_METHODS
+
+
+class ExtendedRights(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS or request.user.is_authenticated
+            and (
+                request.user.is_superuser
+                or request.user.is_moder
+                or request.user.is_admin
+                or request.user == obj.author
+            )
+        )
