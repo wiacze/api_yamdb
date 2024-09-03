bd55f9e6c8db1f290558ed6241006776db7e4dbb
KamilVysotskij <160237836+KamilVysotskij@users.noreply.github.com>
2024-09-04 00:08:00 +0500

    Update urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -38,7 +38,8 @@ router.register(
 )
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comments'
+    CommentViewSet,
+    basename='comments'
 )
 
 auth_urls = [
