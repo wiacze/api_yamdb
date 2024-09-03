38c0877d69861775a93239be8bfcf59c908c9046
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-03 17:49:20 +0300

    Add serializer for comments

---
+++ api_yamdb/api/v1/serializers/comment_serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from reviews.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'author', 'pub_date')
