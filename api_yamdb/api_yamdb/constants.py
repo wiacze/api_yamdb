3e82e6d68ac905904f4c8001a8febdcaadde83e3
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-03 17:44:04 +0300

    Add URL constants

--- api_yamdb/api_yamdb/constants.py
+++ api_yamdb/api_yamdb/constants.py
@@ -2,5 +2,10 @@ NAME_LENGTH = 256
 SLUG_LENGTH = 50
 USERFIELDS_LENGTH = 150
 EMAIL_LENGTH = 254
+
 EMAIL_YAMDB = 'YaMDB@mail.com'
 REGEX = r'^[\w.@+-]+$'
+
+# Адреса для urls.py
+REVIEWS = r'titles/(?P<title_id>\d+)/reviews'
+COMMENTS = r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments'
