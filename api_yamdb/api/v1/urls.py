8604fbd54d455e158006ad5d0ac7e3bc8909ca40
wiacze <151467867+wiacze@users.noreply.github.com>
2024-09-03 17:46:38 +0300

    Register reviews and comments urls

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -4,14 +4,20 @@ from rest_framework.routers import DefaultRouter
 from api.v1.views.category_view import CategoryViewSet
 from api.v1.views.title_view import TitleViewSet
 from api.v1.views.genre_view import GenreViewSet
+from api.v1.views.review_view import ReviewViewSet
+from api.v1.views.comment_view import CommentViewSet
 from api.v1.views.user_view import SignUpViewSet, GetTokenViewSet, UserViewSet
+from api_yamdb.constants import REVIEWS, COMMENTS
 
 
 router = DefaultRouter()
 
+
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
 router.register('titles', TitleViewSet, basename='titles')
+router.register(REVIEWS, ReviewViewSet, basename='reviews')
+router.register(COMMENTS, CommentViewSet, basename='comments')
 router.register('users', UserViewSet, basename='users')
 
 auth_urls = [
