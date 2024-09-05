808a423a93887111139d3a03cfad6b91cae7911b
KamilVysotskij <160237836+KamilVysotskij@users.noreply.github.com>
2024-09-05 22:13:51 +0500

    Update category_view.py

--- api_yamdb/api/v1/views/category_view.py
+++ api_yamdb/api/v1/views/category_view.py
@@ -1,12 +1,12 @@
 from rest_framework import filters
 
-from api.v1.mixins import CustomMixins
+from api.v1.mixins import CustomMixin
 from api.v1.permissions import ReadOnlyOrIsAdmin
 from api.v1.serializers.category_serializer import CategorySerializer
 from reviews.models import Category
 
 
-class CategoryViewSet(CustomMixins):
+class CategoryViewSet(CustomMixin):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (ReadOnlyOrIsAdmin,)
